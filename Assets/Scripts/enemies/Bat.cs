0b85b1a2176f40bef3e3beb925170756f139c7c6
Dragonian707 <jarnodijk2001@hotmail.com>
2022-10-13 12:13:59 +0200

    Enemy AI updated and working

--- Assets/Scripts/enemies/Bat.cs
+++ Assets/Scripts/enemies/Bat.cs
@@ -7,12 +7,19 @@ public class Bat : Enemy
     // Start is called before the first frame update
     public override void Start()
     {
-        
+        health = 2;
+        contactDamage = 1;
+        speed = 3.5f;
+        lookDistance = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckDistance())
+        {
+            // Debug.Log("I see you");
+            FlyToPlayer();
+        }
     }
 }
